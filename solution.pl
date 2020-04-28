ad41892f42356d6d3e9b05a6321424ae716d7626
Furkan Cansever <furkan.cansever@boun.edu.tr>
2020-04-28 23:53:28 +0300

    It's done,but has some stack problems.

--- solution.pl
+++ solution.pl
@@ -11,13 +11,14 @@
 %                                                    Valence, Tempo, DurationMs, TimeSignature]).
 
 
-
+% This is a fact for using only features with value 1.
 features([explicit-0, danceability-1, energy-1,
           key-0, loudness-0, mode-1, speechiness-1,
        	  acousticness-1, instrumentalness-1,
           liveness-1, valence-1, tempo-0, duration_ms-0,
           time_signature-0]).
 
+% This is to filter features whose values are 1.
 filter_features(Features, Filtered) :- features(X), filter_features_rec(Features, X, Filtered).
 filter_features_rec([], [], []).
 filter_features_rec([FeatHead|FeatTail], [Head|Tail], FilteredFeatures) :-
@@ -33,13 +34,14 @@ filter_features_rec([FeatHead|FeatTail], [Head|Tail], FilteredFeatures) :-
 
 
 
-
+% This predicate iterates album ids and returns list of track ids of each album id.
 iterateAlbumIds([],[]).
 iterateAlbumIds([AlbumHead|AlbumTail],[TracksHead|TracksTail]) :-
   album(AlbumHead,_,_,TrackIds),
   TracksHead = TrackIds,
   iterateAlbumIds(AlbumTail,TracksTail).
 
+% This predicate iterates track ids and returns list of track names of each trakcs
 iterateTrackIds([],[]).
 iterateTrackIds([TrackHead|TrackTail],[TrackNameHead|TrackNameTail]) :-
   track(TrackHead,TrackName,_,_,_),
@@ -47,6 +49,7 @@ iterateTrackIds([TrackHead|TrackTail],[TrackNameHead|TrackNameTail]) :-
   iterateTrackIds(TrackTail,TrackNameTail).
 
 
+% This predicate finds all track ids and track names of given artist name by using iterateAlbumIds and iterateTrackIds predicates.
 getArtistTracks(ArtistName, TrackIds, TrackNames) :-
   artist(ArtistName,_,AlbumIds),
   iterateAlbumIds(AlbumIds,TrackIDs),
@@ -55,18 +58,21 @@ getArtistTracks(ArtistName, TrackIds, TrackNames) :-
   
 
 
+% This predicate takes two list and add items of two list and returns new list.
 addTwoList([],[],[]).
 addTwoList(R,[],R).
 addTwoList([H0|T0],[H1|T1],[H2|T2]) :-
   H2 is (H0) + (H1),
   addTwoList(T0,T1,T2).
 
+% This predicates takes a list and a number,after each item of list is divided by given number and return new list.
 divideList([],_,[]).
 divideList([],1,[]).
 divideList([H|T],D,[H1|T1]) :-
   H1 is H/D,
   divideList(T,D,T1).
 
+% This predicate takes track ids list,filters features of track ids,adds their features values and return a new list.
 iterateFeatures([],L,L).
 iterateFeatures([TrackHead|TrackTail],Temp,Result) :-
   track(TrackHead,_,_,_,Features),
@@ -74,7 +80,7 @@ iterateFeatures([TrackHead|TrackTail],Temp,Result) :-
   addTwoList(FilteredFeatures,Temp,L3),
   iterateFeatures(TrackTail,L3,Result).
 
-
+% This predicate takes album id and returns average of features of track ids in given album
 albumFeatures(AlbumId, AlbumFeatures) :-
   album(AlbumId,_,_,IDs),
   length(IDs,Sum),
@@ -82,7 +88,7 @@ albumFeatures(AlbumId, AlbumFeatures) :-
   divideList(AllTracksSum,Sum,AlbumFeatures).
 
 
-
+% This predicate takes artist name,after found all track ids of artist,returns average features of tracks of artist.
 artistFeatures(ArtistName, ArtistFeatures) :-
   artist(ArtistName,_,AlbumIds),
   iterateAlbumIds(AlbumIds,TrackIDs),
@@ -91,12 +97,15 @@ artistFeatures(ArtistName, ArtistFeatures) :-
   iterateFeatures(TrackIds,[],AllTracksSum),
   divideList(AllTracksSum,Sum,ArtistFeatures).
 
+
+% This predicate takes two list contains features values and returns distance of two different features.
 featuresDistance([],[],0).
 featuresDistance([H0|T0],[H1|T1],Sum) :-
   featuresDistance(T0,T1,S),
   Sum is S + (H0-H1)*(H0-H1).
 
 
+% This predicate takes two track ids and returns distance of two track ids.
 trackDistance(TrackId1, TrackId2, Score) :-
   track(TrackId1,_,_,_,Explicit1),
   filter_features(Explicit1,Result1),
@@ -105,6 +114,7 @@ trackDistance(TrackId1, TrackId2, Score) :-
   featuresDistance(Result1,Result2,S),
   Score is sqrt(S).
 
+% This predicate takes two album ids and returns  distance of two album ids.
 albumDistance(AlbumId1, AlbumId2, Score) :-
   albumFeatures(AlbumId1,Result1),
   albumFeatures(AlbumId2,Result2),
@@ -112,7 +122,7 @@ albumDistance(AlbumId1, AlbumId2, Score) :-
   Score is sqrt(S).
 
 
-
+% This predicate take two artist names and returns distance of two artist names.
 artistDistance(ArtistName1, ArtistName2, Score) :-
   artistFeatures(ArtistName1,Result1),
   artistFeatures(ArtistName2,Result2),
@@ -122,7 +132,8 @@ artistDistance(ArtistName1, ArtistName2, Score) :-
 
 
 
-
+% This predicate takes a track id,a list of track list,after comparing track id with track ids of list 
+% and returns a new list contains distance-track id-track name.
 trackSimilarList(_,[],[]).
 trackSimilarList(TrackID,[H|T],[H1|T1]) :-
   TrackId-TrackName = H,
@@ -130,6 +141,7 @@ trackSimilarList(TrackID,[H|T],[H1|T1]) :-
   H1 = Score-TrackId-TrackName,
   trackSimilarList(TrackID,T,T1).
   
+% This predicates takes a list,a number and return a new list from given list until number is 0.
 removeItemsFromList(_,0,[],[]).
 removeItemsFromList([H|T],S,[H1|T1],[H2|T2]) :-
   Score-TrackID-TrackName = H,
@@ -140,12 +152,15 @@ removeItemsFromList([H|T],S,[H1|T1],[H2|T2]) :-
     removeItemsFromList(T,Count,T1,T2)
   ).
 
+% This predicate takes track id and returns most 30 similar track ids and track names according to track distance.
 findMostSimilarTracks(TrackId, SimilarIds, SimilarNames) :-
   findall(TrackID-TrackName,track(TrackID,TrackName,_,_,_),Result),
   trackSimilarList(TrackId,Result,PairList),
   keysort(PairList,SortedPairList),
   removeItemsFromList(SortedPairList,30,SimilarIds,SimilarNames).
   
+% This predicate takes a album id,a list of album list,after comparing album id with album ids of list 
+% and returns a new list contains distance-album id-album name.
 albumSimilarList(_,[],[]).
 albumSimilarList(AlbumID,[H|T],[H1|T1]) :-
   AlbumId-AlbumName = H,
@@ -153,6 +168,7 @@ albumSimilarList(AlbumID,[H|T],[H1|T1]) :-
   H1 = Score-AlbumId-AlbumName,
   albumSimilarList(AlbumID,T,T1).
 
+% This predicates takes a list,a number and return a new list from given list until number is 0.
 removeAlbumsFromList(_,0,[],[]).
 removeAlbumsFromList([H|T],S,[H1|T1],[H2|T2]) :-
   Score-TrackID-TrackName = H,
@@ -163,19 +179,22 @@ removeAlbumsFromList([H|T],S,[H1|T1],[H2|T2]) :-
     removeAlbumsFromList(T,Count,T1,T2)
   ).
 
+% This predicate takes album id and returns most 30 similar album ids and album names according to album distance.
 findMostSimilarAlbums(AlbumId, SimilarIds, SimilarNames) :-
   findall(AlbumID-AlbumName,album(AlbumID, AlbumName,_,_),Result),
   albumSimilarList(AlbumId,Result,PairList),
   keysort(PairList,SortedPairList),
   removeAlbumsFromList(SortedPairList,30,SimilarIds,SimilarNames).
 
-
+% This predicate takes a artist name,a list of artist list,after comparing artist name with artist names of list 
+% and returns a new list contains distance-artist name.
 artistSimilarList(_,[],[]).
 artistSimilarList(ArtistName,[H|T],[H1|T1]) :-
   artistDistance(ArtistName,H,Score),
   H1 = Score-H,
   artistSimilarList(ArtistName,T,T1).
 
+% This predicates takes a list,a number and return a new list from given list until number is 0.
 removeArtistFromList(_,0,[]).
 removeArtistFromList([H|T],S,[H1|T1]) :-
   Score-ArtistName = H,
@@ -186,7 +205,7 @@ removeArtistFromList([H|T],S,[H1|T1]) :-
   ).
 
   
-
+% This predicate takes artist name and returns most 30 similar artist names according to artist distance.
 findMostSimilarArtists(ArtistName, SimilarArtists) :-
   findall(Artists,artist(Artists, _, _),Result),
   artistSimilarList(ArtistName,Result,PairList),
@@ -194,6 +213,7 @@ findMostSimilarArtists(ArtistName, SimilarArtists) :-
   removeArtistFromList(SortedPairList,30,SimilarArtists).
 
 
+% This predicate takes a list of track ids  and return a new list of track ids whose explicit feature is 1.
 filterTracks([],[]).
 filterTracks([H|T],[H1|T1]) :-
   track(H, _, _, _,[E|_]),
@@ -202,17 +222,20 @@ filterTracks([H|T],[H1|T1]) :-
     filterTracks(T,T1)
   ).
 
+% This predicate takes a track list and return filtered list of track ids whose explicit feature is 1.
 filterExplicitTracks(TrackList, FilteredTracks) :-
   filterTracks(TrackList,FilteredTracks).
 
 
 
+% This predicate takes a list of artist name and return genres of these artist names as list.
 trackGenre([],[]).
 trackGenre([H|T],[H1|T1]) :-
   artist(H,Genres,_),
   H1 = Genres,
   trackGenre(T,T1).
 
+% This predicate takes a track id,after find artist name of track id and return genres of found artist names.
 getTrackGenre(TrackId, Genres) :-
   (track(TrackId,_,ArtistNames,_,_) -> trackGenre(ArtistNames,Result),append(Result,Res),list_to_set(Res,Genres);
     trackGenre([],Genres)
@@ -221,14 +244,14 @@ getTrackGenre(TrackId, Genres) :-
 %sub_string(+String,_,_,_,+substring).
 % sub_string("heavy pop",_,_,_,"pop").
  
-  
+% This predicate takes a list and a item and returns true if item is substring of element of list,otherwise false.
 each_contains([],[]) --> false.
 each_contains([H1|T1],Item) :-
   (sub_string(H1,_,_,_,Item) -> true;
     each_contains(T1,Item)
   ).
 
-
+% This predicate takes two list and compares each item of first list with each item of other list for checking substring.
 contains([],[]) --> false.
 contains([H1|T1],[H2|T2]) :- 
   (each_contains([H1|T1],H2) -> true;
@@ -237,11 +260,11 @@ contains([H1|T1],[H2|T2]) :-
   ).
 
 
-
+% This predicate takes four list such that liked,dislike genres,all artistname-genres,given features and with these parameters,
+% comparing genres of artist name with liked and disliked gernes,return new pair list with given condition.
 filterLikedAndDislikeGenre(_,_,[],_,L,L).
 filterLikedAndDislikeGenre(Liked,Dislike,[H3|T3],Features,L2,Result) :-
   ArtistName-Genres = H3,
-  
   
   ((contains(Genres,Liked),not(contains(Genres,Dislike))) -> 
   
@@ -253,6 +276,8 @@ filterLikedAndDislikeGenre(Liked,Dislike,[H3|T3],Features,L2,Result) :-
   ).
 
 %pairList(TrackIds,TrackNames,ArtistName,Result).
+% This predicate takes a list, a features of track, an artist name,after calculating distance of tracks,
+% returns new pair list distance-trackid-trackname-artistname
 pairList([],_,_,L,L).
 pairList([H1|T1],FeaturesOfTrack,ArtistName,L2,Result) :-
   track(H1,TrackName,_,_,Features),
@@ -266,13 +291,14 @@ pairList([H1|T1],FeaturesOfTrack,ArtistName,L2,Result) :-
 
 
 
-
+% This predicate takes a list and remove items from list as given number.
 filterPlayList(_,0,[]).
 filterPlayList([H|T],S,[H|T1]) :-
   Count is -(S,1),
   filterPlayList(T,Count,T1).
 
-
+% This predicate takes a list, iterates list and 
+% return a new 4 lists contains distance,trackids,tracknames and artist name as in different list.
 iteratePlaylist([],[],[],[],[]).
 iteratePlaylist([H1|T1],[H2|T2],[H3|T3],[H4|T4],[H5|T5]) :-
   T-N-A-D = H1,
@@ -280,12 +306,17 @@ iteratePlaylist([H1|T1],[H2|T2],[H3|T3],[H4|T4],[H5|T5]) :-
   iteratePlaylist(T1,T2,T3,T4,T5).
 
 
+% This predicate takes a liked genres list,a disliked genres list,a features list and a filename,after find all artist with 
+% given condition such that liked and disliked genres,filter this artist list,using features calculates distance and sort the list
+% for find most similar playlist.Finally,writes  the most similar track ids,track names,artist name and distance
+% in filename and returns list of track ids.
 discoverPlaylist(LikedGenres,DislikedGenres,Features,FileName,Playlist) :-
   findall(ArtistName-Genres,artist(ArtistName,Genres,_),Result),
   filterLikedAndDislikeGenre(LikedGenres,DislikedGenres,Result,Features,[],PairList),
   keysort(PairList,SortedPairList),
-  filterPlayList(SortedPairList,30,Playlist),
-  iteratePlaylist(Playlist,Distance,TrackIds,TrackNames,ArtistName),
+  filterPlayList(SortedPairList,30,List),
+  iteratePlaylist(List,Distance,TrackIds,TrackNames,ArtistName),
+  Playlist = TrackIds,
   open(FileName,write,Stream),
   writeln(Stream,TrackIds),
   writeln(Stream,TrackNames),
